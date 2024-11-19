24dfbba600ded13591b9c81a6600b88ecebe932a
rohishelke <rohinishelke67@gmail.com>
2024-11-19 16:27:41 +0530

    first code

--- day1.js
+++ day1.js
@@ -1,2 +1,61 @@
 
-console.log("rohini")
+console.log("hello world")
+// Variables are containers for storing values.! Variables hold values, 
+// varibale -- there are three types of variables in js. 
+// var , let , const
+
+// 1. var
+var a = 1
+console.log(a)
+console.log(typeof a) // Number
+
+var b = "rohini"
+console.log(b)
+console.log(typeof b) // String
+
+var c= true
+console.log(c)
+console.log(typeof c) // Boolean
+
+// null - This data type can hold only one possible value that is null.
+// abscence of an object 
+//In JavaScript, null is a primitive data type representing the intentional absence of any object value. 
+//It signifies that a variable is empty or points to no object.
+let d= null 
+console.log(d)
+console.log(typeof d) // object  // abscence of an object 
+
+// undefined 
+//In JavaScript, an undefined variable refers to a variable that has 
+// been declared but has not been assigned a value. Its default value is undefined.
+// 
+
+let e 
+console.log(e)
+/*d="sanchita"
+console.log(d)
+console.log(typeof d)*/
+
+// console.log(f);  // f is not defined (ReferenceError)
+
+// comments 
+// java script supports two types of cooments
+// 1. Single line comment 2. Multi-line comment
+
+//1. Single line Comment - Starts w  
+// used for short notes or disabling a single line code
+
+let x=5;
+console.log(x); // Assign 5 to x
+
+// 2. Multiline Comments 
+// Starts w /* and end with */
+// used for longer explnation or to comment out multiple lines
+
+/*this is multiline comment -
+19th- november 
+This is a wake- up call. 
+You've got to get organize, Rohini. And not just about study , 
+about every details in your life. You've got so much potential. 
+And you just apply it ,
+you could surprise everyone. Most of all, yourself.*/
